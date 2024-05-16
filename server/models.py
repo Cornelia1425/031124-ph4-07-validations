973139b4eaa29320aa9ff22fc656713b6b7329ba
yiranshu <shuyiran1425@hotmail.com>
2024-05-16 14:30:18 -0400

    ya

--- server/models.py
+++ server/models.py
@@ -19,10 +19,50 @@ class User(db.Model, SerializerMixin):
 
     id = db.Column(db.Integer, primary_key=True)
 
-    username = db.Column(db.String)
+    username = db.Column(db.String, nullable=False, unique=True)
     email = db.Column(db.String)
     address = db.Column(db.String)
     phone_number = db.Column(db.String)
     age = db.Column(db.Integer)
     vip = db.Column(db.Boolean)
     year_joined = db.Column(db.Integer)
+
+    # @validates('username')
+    # def validate_username(self, key, value):
+    #     # if len(value.strip()) >= 4:
+    #     #     return value.strip()
+    #     # if len(value.replace(' ', '')) >= 4:
+    #     #     return value.replace(' ', '')
+    #     # if len(value.strip().replace(' ', '_')) >= 4:
+    #     #     return value.strip().replace(' ', '_')
+    #     if len(value.replace(' ', '_').strip()) >= 4:
+    #         return value.replace(' ', '_').strip()
+    #     else:
+    #         raise ValueError('Username must be greater than or equal to 4 characters')
+        
+
+    @validates('username')
+    def validate_unique_username(self, key, value):
+        user = User.query.where(User.username==value).first()
+        if user:
+            raise ValueError('User must be unique!')
+        else:
+            return value
+
+        
+    @validates('age')
+    def validate_age(self, key, value):
+        if value >=13:
+            return value
+        else:
+            raise ValueError('Must be at least 13 years old!')
+    
+    @validates('email')
+    def validate_email(self, key, value):
+        if '$' in value:
+            raise ValueError('Invalid characters for email!')
+        else:
+            return value
+
+    
+    
